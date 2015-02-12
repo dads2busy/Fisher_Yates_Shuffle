2f0888392aeb38cba25c8f281cdac01df440c9af
Aaron Schroeder <dads2busy@gmail.com>
2015-02-11 20:35:56 -0500

    first files

---
+++ FY_Shuffle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Text;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString FY_Shuffle(string seed, string characters)
+    {
+        char[] shuffleArray = characters.ToCharArray();
+
+        Guid guid = new Guid(seed);
+        byte[] gu = guid.ToByteArray();
+        int seedInt = BitConverter.ToInt32(gu, 0);
+
+        Random random = new Random(seedInt);
+
+        for (int i = shuffleArray.Length; i > 1; i--)
+        {
+            // Pick random element to swap.
+            int j = random.Next(i); // 0 <= j <= i-1
+            // Swap.
+            char tmp = shuffleArray[j];
+            shuffleArray[j] = shuffleArray[i - 1];
+            shuffleArray[i - 1] = tmp;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        foreach (char value in shuffleArray)
+        {
+            builder.Append(value);
+        }
+        return builder.ToString();
+    }
+
+}
+
